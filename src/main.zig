47a117d494e7b19a8d8b278001dd9ee977fbbb31
Arwalk <SirAgentProvocateurArwalk@gmail.com>
2023-01-04 17:55:19 +0100

    Message types

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,39 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const MessageType = enum(u4) {
+    Reserved1,
+    CONNECT,
+    CONNACK,
+    PUBLISH,
+    PUBACK,
+    PUBREC,
+    PUBREL,
+    PUBCOMP,
+    SUBSCRIBE,
+    SUBACK,
+    UNSUBSCRIBE,
+    UNSUBACK,
+    PINGREQ,
+    PINGRESP,
+    DISCONNECT,
+    Reserverd2  
+};
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+
+test "Message type values" {
+    try testing.expect(@enumToInt(MessageType.CONNECT) == 1);
+    try testing.expect(@enumToInt(MessageType.CONNACK) == 2);
+    try testing.expect(@enumToInt(MessageType.PUBLISH) == 3);
+    try testing.expect(@enumToInt(MessageType.PUBACK) == 4);
+    try testing.expect(@enumToInt(MessageType.PUBREC) == 5);
+    try testing.expect(@enumToInt(MessageType.PUBREL) == 6);
+    try testing.expect(@enumToInt(MessageType.PUBCOMP) == 7);
+    try testing.expect(@enumToInt(MessageType.SUBSCRIBE) == 8);
+    try testing.expect(@enumToInt(MessageType.SUBACK) == 9);
+    try testing.expect(@enumToInt(MessageType.UNSUBSCRIBE) == 10);
+    try testing.expect(@enumToInt(MessageType.UNSUBACK) == 11);
+    try testing.expect(@enumToInt(MessageType.PINGREQ) == 12);
+    try testing.expect(@enumToInt(MessageType.PINGRESP) == 13);
+    try testing.expect(@enumToInt(MessageType.DISCONNECT) == 14);    
 }
